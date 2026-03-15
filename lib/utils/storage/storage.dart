a5f163a0dd3c4822096ba20f1ff7048f863270b8
систем <oscp.asia@petalmail.com>
2026-03-15 11:17:23 +0800

    ref: save setting storage to setting.json
    设置项现在会存储到与密码相同的目录`getApplicationDocumentsDirectory()`下的 JSON 文件中。

--- lib/utils/storage/storage.dart
+++ lib/utils/storage/storage.dart
@@ -1,26 +1,67 @@
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path/path.dart' as path;
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 
+class JsonSettingStorage {
+  final String filePath;
+  Map<String, dynamic> _data = {};
+
+  JsonSettingStorage(this.filePath);
+
+  Future<void> load() async {
+    final file = File(filePath);
+    if (file.existsSync()) {
+      try {
+        final content = file.readAsStringSync();
+        _data = Map<String, dynamic>.from(jsonDecode(content));
+      } catch (e) {
+        debugPrint('Error loading settings: $e');
+        _data = {};
+      }
+    } else {
+      _data = {};
+    }
+  }
+
+  Future<void> save() async {
+    final file = File(filePath);
+    await file.writeAsString(jsonEncode(_data));
+  }
+
+  T get<T>(String key, {required T defaultValue}) {
+    final value = _data[key];
+    if (value == null) return defaultValue;
+    return value as T;
+  }
+
+  Future<void> put(String key, dynamic value) async {
+    _data[key] = value;
+    await save();
+  }
+
+  Future<void> delete(String key) async {
+    _data.remove(key);
+    await save();
+  }
+
+  Set<String> get keys => _data.keys.toSet();
+}
+
 abstract final class GStorage {
-  static late final Box<dynamic> setting;
-  static late final Box<dynamic> localCache;
+  static late final JsonSettingStorage setting;
+  static late final JsonSettingStorage localCache;
 
   static Future<void> init() async {
-    // 获取应用支持目录
-    final dir = await getApplicationSupportDirectory();
-    await Hive.initFlutter(path.join(dir.path, 'hive'));
+    final dir = await getApplicationDocumentsDirectory();
+
+    setting = JsonSettingStorage('${dir.path}/setting.json');
+    localCache = JsonSettingStorage('${dir.path}/localCache.json');
 
     await Future.wait([
-      // 设置
-      Hive.openBox('setting').then((res) => setting = res),
-      // 本地缓存
-      Hive.openBox(
-        'localCache',
-        compactionStrategy: (int entries, int deletedEntries) {
-          return deletedEntries > 4;
-        },
-      ).then((res) => localCache = res),
+      setting.load(),
+      localCache.load(),
     ]);
   }
 }
